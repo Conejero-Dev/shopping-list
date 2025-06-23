da071f662af68fd4d8110902d687a977c13e6dfb
Javier Fajardo <20287019+Conejero-Dev@users.noreply.github.com>
2025-06-23 17:34:29 +0100

    Add removeItemFromStorage and update few functions

--- script.js
+++ script.js
@@ -68,18 +68,39 @@ function getItemsFromStorage() {
     return itemsFromStorage;
 }
 
-function removeItem(e) {
+function onClickItem(e) {
     if (e.target.parentElement.classList.contains('remove-item')) {
-        if (confirm('Are you sure?')) {
-            e.target.parentElement.parentElement.remove();
-            checkUI();
-        }
+        removeItem(e.target.parentElement.parentElement);
     }
+}
+
+function removeItem(item) {
+    if (confirm('Are you sure?')) {
+        // Remove item from DOM
+        item.remove();
+        // Remove item from storage
+        removeItemFromStorage(item.textContent);
+        checkUI();
+    }
+}
+
+function removeItemFromStorage(item) {
+    let itemsFromStorage = getItemsFromStorage();
+    // Filter out item to be removed
+    itemsFromStorage = itemsFromStorage.filter((i) => i !== item);
+    // Re-set localStorage
+    itemsFromStorage.length < 1
+        ? localStorage.clear()
+        : localStorage.setItem('items', JSON.stringify(itemsFromStorage));
 }
 
 function clearItems() {
     while (itemList.firstChild) {
         itemList.removeChild(itemList.firstChild);
+    }
+    // Clear from localStorage
+    if (confirm('Are you sure?')) {
+        localStorage.removeItem('items');
     }
     checkUI();
 }
@@ -112,7 +133,7 @@ function filterItems(e) {
 function init() {
     // Event Listeners
     itemForm.addEventListener('submit', onAddItemSubmit);
-    itemList.addEventListener('click', removeItem);
+    itemList.addEventListener('click', onClickItem);
     clearBtn.addEventListener('click', clearItems);
     itemFilter.addEventListener('input', filterItems);
     document.addEventListener('DOMContentLoaded', displayItems);
